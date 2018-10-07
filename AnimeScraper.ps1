df2321b497e930adec65a39002202764f01f65b9
jkiernan <jkiernan53@gmail.com>
2018-10-07 11:17:27 -0400

    Added logging

--- AnimeScraper.ps1
+++ AnimeScraper.ps1
@@ -1,11 +1,14 @@
 ﻿If ($env:COMPUTERNAME -eq "JPK-HTPC") {
     $Script:DownloadDirectory = "D:\Seedbox\Completed_Downloads"
-    $Script:AnimeList = Get-Content ".\AnimeList.txt"
+    $Script:AnimeList = Get-Content "C:\Github\PowerShell\AnimeList.txt"
+    $Script:LogFile = "C:\Github\PowerShell\AnimeScraperLog.log"
 }
 If ($env:COMPUTERNAME -eq "JPK-PC2") {
     $Script:DownloadDirectory = "Z:\Completed_Downloads"
     $Script:AnimeList = Get-Content "D:\Dropbox\Your team Dropbox\James Kiernan\Computer\Documents\GitHub\PowerShell\AnimeList.txt"
+    $Script:LogFile = "D:\Dropbox\Your team Dropbox\James Kiernan\Computer\Documents\GitHub\PowerShell\AnimeScraperLog.log"
 }
+Start-Transcript -Path $LogFile
 $FolderPath = Get-ChildItem -Path "Y:" | Select -ExpandProperty Name
 $Downloads = Get-ChildItem -Path $DownloadDirectory -Filter "`[HorribleSubs`]*.mkv" -Recurse | Select -exp FullName
 foreach ($Episode in $Downloads) {
@@ -32,3 +35,4 @@ foreach ($Episode in $Downloads) {
         }
     }
 }
+Stop-Transcript
